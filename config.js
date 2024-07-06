0b0e7a0bbc1f41b520e17c05cee36c4ab7601e60
mohamad3553 <mohamadmostafat2@gmail.com>
2024-07-06 22:40:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,20 +2,20 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
-global.owner = [['97251297441', '🌩️maiky 👑', true], 
- ['97251297441', '🌩️maiky 👑', true],  ['97251297441'], ['97251297441'], ['97251297441']
+global.owner = [['48699524591', '🌩️maiky 👑', true], 
+ ['48699524591', '🌩️maiky 👑', true],  ['48699524591'], ['48699524591'], ['48699524591']
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
 global.botNumberCode = '' //Ejemplo: +97251297441
 global.confirmCode = ''
 
-global.animxscans = ['97251297441']
-global.suittag = ['97251297441']
+global.animxscans = ['48699524591']
+global.suittag = ['48699524591']
 global.mods = []
 global.prems = []
 
-global.packname = '『 97251297441 』'
+global.packname = '『 48699524591 』'
 global.author = '『 غوكـو بــ☠️ـوت 』'
 global.wm = '『غـوكـو بــ🐍ـؤؤت 』'
 global.wm2 = '『 غــوكـو بــ💎ـوت 』'
@@ -29,7 +29,7 @@ global.lenguaje = 'Español'
 global.KMA = '╰━━━〔 *🛡️ 1.7.9* 〕━━━━━⬣'
 global.menudi = ['⛶','❏','⫹⫺']
 global.dev = '© 𝑮𝒐𝒌𝒖_BOT'
-global.devnum = '97251297441'
+global.devnum = '48699524591'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => { unwatchFile(file)
